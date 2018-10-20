bf0932578b6aca3d35e7879b7eec5c501dc2b08f
jlansineva <30691995+jlansineva@users.noreply.github.com>
2018-10-20 12:37:35 +0300

    Add files via upload

---
+++ Assets/Editor/SplatterTargetEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SplatterTarget))]
+public class SplatterTargetEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var inspectedItem = target as SplatterTarget;
+
+        inspectedItem.color = EditorGUILayout.ColorField("Color", inspectedItem.color);
+        inspectedItem.detail = EditorGUILayout.IntSlider("Detail Level", inspectedItem.detail, 2, 12);
+        EditorGUILayout.LabelField("Splatter Resolution", string.Format("{0}x{0}", inspectedItem.pow2Size));
+        inspectedItem.size = EditorGUILayout.FloatField("Object Size", inspectedItem.size);
+    }
+}
