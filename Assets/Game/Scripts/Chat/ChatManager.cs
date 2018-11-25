0da5d6a546d58cea3bf92acb1ded5e0585eb2bff
Ville Heikkinen <wildhexes@gmail.com>
2018-11-25 13:33:44 +0200

    start implementing online chat

--- Assets/Game/Scripts/Chat/ChatManager.cs
+++ Assets/Game/Scripts/Chat/ChatManager.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class ChatManager : MonoBehaviour {
+public class ChatManager : NetworkBehaviour  {
 
     public string username;
 
