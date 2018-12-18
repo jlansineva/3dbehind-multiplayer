05be41873fda5e99964530a7f276adc39c5418ae
Tuukka Kurtti <tuukkak123@gmail.com>
2018-12-18 22:40:15 +0200

    Add a way to configure network address by text file

--- Assets/Game/Scripts/BehindNetworkManager.cs
+++ Assets/Game/Scripts/BehindNetworkManager.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class BehindNetworkManager : NetworkManager
 {
-    public GameObject clientCamera;
+    public void Start()
+    {
+        networkAddress = File.ReadAllText("address.txt");
+    }
 
     public override void OnClientConnect(NetworkConnection conn)
     {
         base.OnClientConnect(conn);
     }
 }
+    
